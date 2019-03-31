6ac82fe6f558c76e911cd9d589fb8c7d4e3a854a
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-03-31 16:13:44 -0500

    cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,19 +6,18 @@ for (var i = 0; i < 4; i++){
     
     var crystal = $("<div>");
     crystal.attr("class", 'crystal');
-console.log(crystal)
+// console.log(crystal)
     $(".crystals").append(crystal);
   
 }
 
-//4 crystals
+//4 crystals - done
 //random numbers for crystals
 //new number every game to the 4 crystals
 //number should be from 1 -10
 //random number to match from 1-100
 //clicking crystals to equal the random number made.
 //if not equal to random number lose
-//if equal to randon number win
+//if equal to random number win
 //keep track of wins or losses 
 //reset after win or loss but keep scores
-
