3d92f7a70385d4493e306695276d58a7d09d85fb
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-03-31 17:37:56 -0500

    random number for user to match made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,26 @@
-var win;
-var lost;
-var randomNumber;
+
+var winScore;
+var lossesScore;
+var match;
+
+
+//random number to match from 30-99
+randomMatch = Math.floor((Math.random() * 69) + 30);
+$("#match").html('Try to match: ' + randomMatch); 
 //4 crystals - done
 for (var i = 0; i < 4; i++){
 //random numbers for crystals - done
+//number should be from 1 -10
     var random = Math.floor((Math.random()* 12 ) + 1);
     console.log(random);
     var crystal = $("<div>");
-    crystal.attr("class", 'crystal');
+//new number every game to the 4 crystals
+    crystal.attr({"class": 'crystal', "data-random":random});
 // console.log(crystal)
     $(".crystals").append(crystal);
   
 }
 
-//4 crystals - done
-//random numbers for crystals
-//new number every game to the 4 crystals
-//number should be from 1 -10
-//random number to match from 1-100
 //clicking crystals to equal the random number made.
 //if not equal to random number lose
 //if equal to random number win
