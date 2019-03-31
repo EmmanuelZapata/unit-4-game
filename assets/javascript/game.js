03a5d7d4d8d0bc85d0fe7f08241863c3201857a7
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-03-31 15:52:42 -0500

    moved javescript into the body of html... who knew that would fix everything.

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+var win;
+var lost;
+var randomNumber;
+
+for (var i = 0; i < 4; i++){
+    
+    var crystal = $("<div>");
+    crystal.attr("class", 'crystal');
+console.log(crystal)
+    $(".crystals").append(crystal);
+  
+}
+
+//4 crystals
+//random numbers for crystals
+//new number every game to the 4 crystals
+//number should be from 1 -10
+//random number to match from 1-100
+//clicking crystals to equal the random number made.
+//if not equal to random number lose
+//if equal to randon number win
+//keep track of wins or losses 
+//reset after win or loss but keep scores
+
