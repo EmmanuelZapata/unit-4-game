48676f385713f8db0aa4bb099b66d92ef893d745
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-04-01 00:02:58 -0500

    document ready added and values for var of statment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-
-var winScore;
-var lossesScore;
-var match;
+$(document).ready(function() {
+var winScore = 0;
+var lossesScore = 0;
+var match = 0;
 var crystalNum = 0;
 
 //random number to match from 30-99
@@ -14,19 +14,18 @@ for (var i = 0; i < 4; i++){
     var random = Math.floor((Math.random()* 12 ) + 1);
     console.log(random);
     var crystal = $("<div>");
-//new number every game to the 4 crystals
+//new number every game to the 4 crystals - done
     crystal.attr({"class": 'crystal', "data-random":random});
 // console.log(crystal)
     $(".crystals").append(crystal);
   
 }
-//clicking crystals to equal the random number made.
+//clicking crystals to equal the random number made. - done
 $(".crystal").click(function (){ 
 
- var crystalNum = $(this).attr('data-random');
+ var crystalNum = parseInt($(this).attr('data-random'));
 console.log(crystalNum);
 });
-//clicking crystals to equal the random number made.
 //if not equal to random number lose
 //if equal to random number win
 //keep track of wins or losses 
