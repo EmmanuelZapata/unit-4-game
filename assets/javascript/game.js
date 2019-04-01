ec771550da2692b8cea4649356f02cdbce238ddd
EmmanuelZapata <zapata.emmanuel@gmail.com>
2019-04-01 00:22:18 -0500

    changed to make every crystal have it's own value.  Fix javascript first then html.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
-var winScore = 0;
-var lossesScore = 0;
-var match = 0;
-var crystalNum = 0;
+	var scoreMatch;
+	var wins = 0;
+	var losses = 0;
+	var crystalValue1;
+	var crystalValue2;
+	var crystalValue3;
+	var crystalValue4;
+	var userScore;
 
 //random number to match from 30-99
-randomMatch = Math.floor((Math.random() * 69) + 30);
-$("#match").html('Try to match: ' + randomMatch); 
-//4 crystals - done
-for (var i = 0; i < 4; i++){
-//random numbers for crystals - done
-//number should be from 1 -10
-    var random = Math.floor((Math.random()* 12 ) + 1);
-    console.log(random);
-    var crystal = $("<div>");
-//new number every game to the 4 crystals - done
-    crystal.attr({"class": 'crystal', "data-random":random});
-// console.log(crystal)
-    $(".crystals").append(crystal);
+function startGame (){
+    userScore = 0;
+
+    // Getting a random number between 19-120 for the game 
+    scoreMatch = 19 + (Math.floor(Math.random()*102));
+
+    // Getting random values for each crystal between 1-12
+    crystalValue1 = Math.floor(1 + (Math.random()*12));
+    crystalValue2 = Math.floor(1 + (Math.random()*12));
+    crystalValue3 = Math.floor(1 + (Math.random()*12));
+    crystalValue4 = Math.floor(1 + (Math.random()*12));
   
 }
 //clicking crystals to equal the random number made. - done
@@ -25,8 +27,10 @@ $(".crystal").click(function (){
 
  var crystalNum = parseInt($(this).attr('data-random'));
 console.log(crystalNum);
-});
+
 //if not equal to random number lose
 //if equal to random number win
 //keep track of wins or losses 
 //reset after win or loss but keep scores
+
+})});
